6f53490caccf0f1e46b7c03d7f2b26425555d4a2
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-02-06 13:01:22 -0600

    commiting before push to main

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,35 +2,62 @@ import React from "react";
 import { Container, Grid, Typography, Link } from "@mui/material";
 
 function Footer() {
+  // Define an array of links for easy mapping
+  // this makes an easier edit in the future
+  const linkSections = [
+    { label: "Home", path: "/home" },
+    { label: "Desk", path: "/desk" },
+    { label: "Gallery", path: "/gallery" },
+    { label: "About", path: "/about" },
+  ];
+
+  // Define an array for the company info sections
+  // this makes an easier edit in the future
+  // TODO make these links like above? maybe just all to my github or something?
+  const infoSections = [
+    "Newsletter Sign-up",
+    "Contact Information",
+    "Legal",
+    "Accessibility",
+    "Awards/Certifications",
+    "Copyright Notice",
+  ];
+
   return (
-    <Container component="footer" sx={{ padding: "20px 0" }}>
+    <Container component="footer" style={{ padding: "20px 0" }}>
       <Grid container justifyContent="space-between" alignItems="center">
-        {/* Production Notice: Use Typography for semantic and accessible text rendering */}
-        <Grid item xs={12} sm={4}>
-          <Typography variant="subtitle1" sx={{ textAlign: { xs: 'center', sm: 'left' } }}>
+        {/* Left side - Production Notice */}
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle1">
             StorySphere is a Ty Domben production
           </Typography>
         </Grid>
 
-        {/* Navigation Links: Using Grid to organize links into a responsive layout */}
-        <Grid item xs={12} sm={8}>
-          <Grid container spacing={2} justifyContent="flex-end" alignItems="center">
-            {/* Mapping over an array of link objects could be more maintainable */}
-            {['Home', 'Desk', 'Gallery', 'About'].map((text) => (
-              <Grid item key={text}>
-                <Link href={`/${text.toLowerCase()}`} variant="body2">
-                  {text}
+        {/* Right side - Navigation Links */}
+        <Grid item xs={12} sm={6}>
+          {/* Stacking links vertically */}
+          <Grid container direction="column" spacing={1}>
+            {linkSections.map((section) => (
+              <Grid item key={section.label}>
+                <Link href={section.path} variant="body2">
+                  {section.label}
                 </Link>
               </Grid>
             ))}
-
-            {/* Additional Information: Placed in a single block for now, but should be split if they are separate links */}
-            <Grid item xs={12} sx={{ textAlign: 'center', mt: { sm: 2, xs: 1 } }}>
-              <Typography variant="body2">
-                [Newsletter Sign-up] [Contact Information] [Legal] [Accessibility] [Awards/Certifications] [Copyright Notice]
-              </Typography>
-            </Grid>
           </Grid>
+        </Grid>
+
+        {/* Company Info Sections */}
+
+        {/* Aligns items to the end of the grid container (right side) */}
+        {/* TODO : Stacking links vertically  I've spent enough time on this for right now */}
+        <Grid container direction="column" justifyContent="flex-end" spacing={1}>
+          {/* The map function will render each item in the infoSections array */}
+          {infoSections.map((info) => (
+            <Grid item key={info}>
+              <Typography variant="body2">{info}</Typography>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Container>
